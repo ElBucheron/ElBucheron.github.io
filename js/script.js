ad558522acf145693a9143805b1da75a082f3554
ElBucheron <reyne.ugo@gmail.com>
2019-03-25 13:05:00 +0000

    Testing Cross domain policy

--- js/script.js
+++ js/script.js
@@ -10,3 +10,50 @@ function showCat(id) {
 		elem.className = "content hidden";
 	}
 }
+
+function readTextFile(filepath) {
+	var str = "";
+	
+	$.get(filepath, function(data) {    
+	    var lines = data.split("\n");
+
+	    $.each(lines, function(n, elem) {
+	       $('#modal-content').append('<div>' + elem + '</div>');
+	    });
+	});
+
+	return str;
+}
+
+function openModal(title, path) {
+	var modal = document.getElementById("modal");
+	modal.style.display = "block";
+
+	var modalContent = document.getElementById("modal-content");
+	var titleDiv = document.createElement("h1");
+	titleDiv.style.textAlign = "center";
+	titleDiv.appendChild(document.createTextNode(title));
+    modalContent.appendChild(titleDiv);
+
+    var text = readTextFile(path);
+
+    var p = document.createElement("p");
+    p.appendChild(document.createTextNode(text))
+    modalContent.appendChild(p);
+}
+
+function closeModal() {
+	var modal = document.getElementById("modal");
+	modal.style.display = "none";
+
+	var modalContent = document.getElementById("modal-content");
+	while (modalContent.lastChild.className != "close") {
+    	modalContent.removeChild(modalContent.lastChild);
+	}
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+}
