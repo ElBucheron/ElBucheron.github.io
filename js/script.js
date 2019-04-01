e51ef9b67c749b57276930ff9ad0c8296060d333
ElBucheron <reyne.ugo@gmail.com>
2019-04-02 00:56:27 +0100

    Background and theme modified

--- js/script.js
+++ js/script.js
@@ -92,3 +92,11 @@ window.onclick = function(event) {
 		closeModal();
 	}
 }
+
+$(document).ready(function() {
+  var $magic = $(".magic"),
+      magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function(e) {
+    $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+  });
+});
