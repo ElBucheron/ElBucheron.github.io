3f39b4c95ae77175eb33d3c46993facce83c0514
ElBucheron <reyne.ugo@gmail.com>
2019-04-01 01:31:40 +0100

    Changed logo and expand categories

--- js/script.js
+++ js/script.js
@@ -5,10 +5,54 @@ function showCat(id) {
 	if (elem.className == "content hidden") {
 		elem.style.display = "block";
 		elem.className = "content shown";
+		elem.previousElementSibling.firstChild.style.transform = 'rotate(90deg)';
 	} else {
 		elem.style.display = "none";
 		elem.className = "content hidden";
+		elem.previousElementSibling.firstChild.style.transform = 'rotate(0deg)';
 	}
+}
+
+function expandAll() {
+	var btn = document.getElementById('actionAll');
+	var elem = document.getElementById('notes');
+
+	if (btn.className == "retracted") {
+		$.each(elem.children, function(n, e){
+			if (e.id.match(/cat/g)) {
+				e.style.display = "block";
+				e.className = "content shown";
+				e.previousElementSibling.firstChild.style.transform = 'rotate(90deg)';
+			}
+		});
+		btn.textContent = "| Retract All |";
+		btn.className = "expanded";
+	} else {
+		$.each(elem.children, function(n, e){
+			if (e.id.match(/cat/g)) {
+				e.style.display = "none";
+				e.className = "content hidden";
+				e.previousElementSibling.firstChild.style.transform = 'rotate(0deg)';
+			}
+		});
+		btn.textContent = "| Expand All |";
+		btn.className = "retracted";
+	}
+
+	$.each(elem.children, function(n, e){
+		if (e.id.match(/cat/g)) {
+			if (e.className == "content hidden") {
+				e.style.display = "block";
+				e.className = "content shown";
+				e.previousElementSibling.firstChild.style.transform = 'rotate(90deg)';
+			} else {
+				e.style.display = "none";
+				e.className = "content hidden";
+				e.previousElementSibling.firstChild.style.transform = 'rotate(0deg)';
+			}
+		}
+	});
+	btn.textContent = "Expand All";
 }
 
 function openModal(filepath) {
@@ -59,7 +103,7 @@ function closeModal() {
 }
 
 window.onclick = function(event) {
-  if (event.target == modal) {
-    closeModal();
-  }
+	if (event.target == modal) {
+		closeModal();
+	}
 }
