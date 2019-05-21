24b67c53858a4adfdd6d12258997f4b2faa6c3e4
ElBucheron <elbucheron@pop-os.localdomain>
2019-05-21 20:29:55 +0100

    Changed theme of notes

--- js/markDown.js
+++ js/markDown.js
@@ -1,23 +1,3 @@
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-function typeWrite(span){
-  $('#'+span).addClass('cursor')
-  var text = $('#'+span).text();
-  var randInt = 0
-  for (var i = 0; i < text.length; i++) {
-    randInt += parseInt(randomIntFromInterval(40,300));
-    var typing = setTimeout(function(y){
-      $('#'+span).append(text.charAt(y));
-    },randInt, i);
-  };
-  setTimeout(function(){
-    $('#'+span).removeClass('cursor');
-  },randInt+2500);
-}
-
 function parseMd(md){
 
   //ul
