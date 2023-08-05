6b555fecff80d96cfc4c740eda96383125b9e464
ElBucheron <reyne.ugo@gmail.com>
2023-08-05 14:27:41 +0200

    Update scam-list.js

--- js/scam-list.js
+++ js/scam-list.js
@@ -3,11 +3,12 @@ const scams = [
     title: 'Phishing',
     description: 'Phishing scams involve fake emails or websites that trick users into providing sensitive information.',
     details: 'Attackers may impersonate legitimate companies or individuals to deceive victims into revealing their login credentials, financial information, or other personal data. Be cautious when clicking on links in emails and always verify the sender\'s identity before providing any sensitive information.',
+    image: 'phishing.jpg',
   },
   {
     title: 'Identity Theft',
     description: 'Identity theft occurs when someone steals your personal information to commit fraud or other crimes.',
     details: 'Criminals may use your stolen information to open new accounts, make purchases, or file fraudulent tax returns in your name. To protect yourself, monitor your credit reports regularly, use strong passwords, and be cautious when sharing personal information online.',
-  },
-  // Add more scams here
+    image: 'identity_theft.jpg',
+  }
 ];
