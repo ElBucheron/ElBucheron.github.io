def706e90e90b35bf1c9293ba9692b05a184ce3d
ElBucheron <reyne.ugo@gmail.com>
2023-08-05 16:28:01 +0200

    Update scam-list.js

--- js/scam-list.js
+++ js/scam-list.js
@@ -1,50 +1,18 @@
 const scams = [
   {
+    id: 'phishing',
     title: 'Phishing',
     description: 'Phishing scams involve fake emails or websites that trick users into providing sensitive information.',
-    details: 'Attackers may impersonate legitimate companies or individuals to deceive victims into revealing their login credentials, financial information, or other personal data. Be cautious when clicking on links in emails and always verify the sender\'s identity before providing any sensitive information.',
+    example: 'Attackers may impersonate legitimate companies or individuals to deceive victims into revealing their login credentials, financial information, or other personal data.',
+    solution: 'Be cautious when clicking on links in emails and always verify the sender\'s identity before providing any sensitive information.',
     image: 'phishing.jpg',
   },
   {
+    id: 'identity-theft',
     title: 'Identity Theft',
     description: 'Identity theft occurs when someone steals your personal information to commit fraud or other crimes.',
-    details: 'Criminals may use your stolen information to open new accounts, make purchases, or file fraudulent tax returns in your name. To protect yourself, monitor your credit reports regularly, use strong passwords, and be cautious when sharing personal information online.',
+    example: 'Criminals may use your stolen information to open new accounts, make purchases, or file fraudulent tax returns in your name.',
+    solution: 'Monitor your credit reports regularly, use strong passwords, and be cautious when sharing personal information online.',
     image: 'identity_theft.jpg',
-  },
-  {
-    title: 'Investment Scams',
-    description: 'Investment scams involve fraudulent schemes that promise high returns with little or no risk.',
-    details: 'Scammers may call or email you with investment opportunities that seem too good to be true, or pressure you to invest quickly before the opportunity disappears. Do your research before investing, and be wary of unsolicited investment offers or high-pressure sales tactics.',
-    image: 'investment_scams.jpg',
-  },
-  {
-    title: 'Pyramid Schemes',
-    description: 'Pyramid schemes involve recruiting new members to make money, rather than selling a legitimate product or service.',
-    details: 'Scammers may promise large profits for recruiting new members, but the scheme eventually collapses when there are not enough new members to sustain it. Avoid any scheme that requires you to recruit new members to make money, and be wary of any opportunity that promises large profits with little effort.',
-    image: 'pyramid_schemes.jpg',
-  },
-  {
-    title: 'Fake Goods Scams',
-    description: 'Fake goods scams involve selling counterfeit or fake products at a cheaper price.',
-    details: 'Scammers may use scam adverts on social media or online marketplaces to sell fake products, such as designer clothing, electronics, or pharmaceuticals. Buy products only from reputable sellers and websites, and be wary of deals that seem too good to be true.',
-    image: 'fake_goods_scams.jpg',
-  },
-  {
-    title: 'Charity Scams',
-    description: 'Charity scams involve impersonating a legitimate charity or mutual aid group to solicit donations or personal information.',
-    details: 'Scammers may offer to do shopping or cleaning for you, or ask for donations to a fake charity or mutual aid group. Verify the legitimacy of any charity or mutual aid group before donating or sharing personal information, and be wary of unsolicited requests for donations.',
-    image: 'charity_scams.jpg',
-  },
-  {
-    title: 'Tech Support Scams',
-    description: 'Tech support scams involve impersonating a legitimate tech support company to trick users into paying for unnecessary services or software.',
-    details: 'Scammers may call or send pop-up messages claiming that your computer has a virus or other problem, and offer to fix it for a fee. Do not provide any personal or financial information to unsolicited callers or pop-up messages, and use reputable antivirus software to protect your computer.',
-    image: 'tech_support_scams.jpg',
-  },
-  {
-    title: 'Fake Antivirus Software Scams',
-    description: 'Fake antivirus software scams involve ads or pop-ups that claim your computer is infected with a virus and offer to fix it with their software.',
-    details: 'Scammers may use scare tactics to make you believe that your computer is infected with a virus or other malware, and pressure you to download their software. Use reputable antivirus software and do not download or install any software from unsolicited sources.',
-    image: 'fake_antivirus_scams.jpg',
   }
 ];
